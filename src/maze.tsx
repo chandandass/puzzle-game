e83559d39ab8c1d3fccafa581be3c2c4d559f735
chandandass <cdas99633@gmail.com>
2024-06-10 12:21:33 +0530

    maze path final

--- src/maze.tsx
+++ src/maze.tsx
@@ -2,17 +2,19 @@ import React, { useRef, useEffect, useState } from 'react';
 import { View, Dimensions, StyleSheet, Button, Image as RNImage, PixelRatio, Text } from 'react-native';
 import { GestureHandlerRootView, PanGestureHandler, State } from 'react-native-gesture-handler';
 import Canvas, { Image as CanvasImage, CanvasRenderingContext2D } from 'react-native-canvas';
-import { mageImage } from './mageimage';
 import { pathImage } from './path';
 import { frog } from './frog';
 import { dragonfly } from './dragonfly';
-import { mazePath } from './mazePath';
+import Toast from 'react-native-simple-toast';
 
 let { width: displayWidth, height: displayHeight } = Dimensions.get('window');
 const PATH_COLOR = '#F9F7DC'
-const startPoint = { x: 85 , y: 424 }
-const endPoint = { x: 165, y: 633 }
-console.log("displayWidth: ", displayWidth, "displayHeight: ", displayHeight)
+const startPoint = { x: 8.9 , y: 9.82 }
+const endPoint = { x: 87.79, y: 218.82, w: 0, h: 0 }
+const anim1Diff = {x: -.06, y: 0};
+const anim2Diff = {x: 0.04, y: -.05};
+const Title = 'Help Meghuchi find the way to the dragonfly';
+
 const CanvasComponent: React.FC = () => {
   const backgroundCanvasRef = useRef<Canvas | null>(null);
   const drawingCanvasRef = useRef<Canvas | null>(null);
@@ -21,9 +23,11 @@ const CanvasComponent: React.FC = () => {
   let imageDim = {width: 0, height: 0};
   let finalStartPoint = {x: 0, y: 0};
   let finalEndPoint = {x: 0, y: 0, w: 0, h: 0};
+  let initialStartPoint = {x:0,y:0};
   let xyDiff = {x: 0, y: 0};
   let isDrawing = false;
   let isDrawingStop = false;
+  const touchDistance = ((displayHeight < displayWidth) ? displayHeight : displayWidth) * .3;
 
   useEffect(() => {
     const backgroundCanvas = backgroundCanvasRef.current;
@@ -31,10 +35,7 @@ const CanvasComponent: React.FC = () => {
       backgroundCanvas.width = displayWidth;
       backgroundCanvas.height = displayHeight;
       backgroundContext = backgroundCanvas.getContext('2d');
-      loadImage(backgroundContext);
-      // backgroundContext.arc(displayWidth * .15, displayHeight * .55, 5, 0,  2 * Math.PI)
-      // backgroundContext.fill();
-      
+      loadImage(backgroundContext);  
     }
     
     const drawingCanvas = drawingCanvasRef.current;
@@ -43,9 +44,8 @@ const CanvasComponent: React.FC = () => {
       drawingCanvas.height = displayHeight;
       drawingContext = drawingCanvas.getContext('2d');
       if (drawingContext) {
-        drawingContext.lineWidth = 1.5;
+        drawingContext.lineWidth = 1;
         drawingContext.strokeStyle = 'red';
-        drawingContext.fillStyle = 'green'
       }
     }
   }, []);
@@ -113,8 +113,7 @@ const CanvasComponent: React.FC = () => {
       image.src = pathImage;
 
       image.addEventListener('load', () => {
-        // const pathContainer = { width: displayWidth * .7, height: displayHeight * .5}
-        const pathContainer = { width: 360, height: displayHeight * .5}
+        const pathContainer = { width: displayWidth * .7, height: displayHeight * .6}
         const imageAR = image.width / image.height;
         const containerAr = pathContainer.width / pathContainer.height;
         if(imageAR > containerAr) {
@@ -122,35 +121,32 @@ const CanvasComponent: React.FC = () => {
           } else {
           imageDim = {width: pathContainer.height, height: pathContainer.height * imageAR};
         }
- 
-        console.log("imageAr", imageAR)
-        console.log("imageDim: ", imageDim);
-        // imageDim = {width: displayWidth * widthPercent, height: (displayWidth * widthPercent) / imageAR};
-        // console.log("half of wp: ", (1-widthPercent)/2)
-        // const pathStart = {x: displayWidth * ((1 - widthPercent)/2), y: displayWidth *  ((heightRatio * 3.5))}
-        const heightRatio = (displayHeight / imageDim.height);
-        const widthRatio = (displayWidth / imageDim.width);
+        const heightRatio = (imageDim.height / image.height);
+        const widthRatio = (imageDim.width / image.width);
         const imageStarting = {x: (displayWidth - imageDim.width) / 2, y: displayHeight * .8  - imageDim.height}
-        console.log("imageDim: ", imageDim);
-        finalStartPoint = {x: (startPoint.x * widthRatio), y: (startPoint.y * heightRatio)};
-        finalEndPoint = {x: endPoint.x * widthRatio, y: endPoint.y * heightRatio, w: 10, h: 10};
-        // drawingContext.fillRect(displayWidth * .2 + (((displayWidth * .7) / imageDim.width ) * 231.9), displayHeight * .55 + (((displayWidth * .7) / imageAR) / imageDim.height * 38.82), 10,10)
-        // context.drawImage(image, displayWidth * .2, displayHeight * .9 - (displayWidth * widthPercent) / imageAR, displayWidth * widthPercent, (displayWidth * widthPercent) / imageAR); 
+        finalStartPoint = {x: imageStarting.x + (startPoint.x * widthRatio), y: imageStarting.y + (startPoint.y * heightRatio)};
+        initialStartPoint = {x: finalStartPoint.x, y: finalStartPoint.y};
+        finalEndPoint = {x: imageStarting.x + (endPoint.x * widthRatio), y: imageStarting.y + (endPoint.y * heightRatio), w: 10, h: 10};
+        // drawingContext.fillRect(finalStartPoint.x, finalStartPoint.y , 10,10)
+        // drawingContext.fillRect(finalEndPoint.x, finalEndPoint.y , 10,10)
         context.drawImage(image,  imageStarting.x, imageStarting.y, imageDim.width, imageDim.height); 
         
         anim1.src = frog;
         anim2.src = dragonfly;
-        const anim1Diff = {x: -0.1, y: 0};
-        const anim2Diff = {x: 0, y: 0};
+
         anim1.addEventListener('load', ()=> {
+          const anim1AR = anim1.width / anim1.height;
+          const anim1RenderDim  = {width: displayHeight * .1 / anim1AR , height: displayHeight * .1};
           console.log("anim1")
-          context.drawImage(anim1, imageStarting.x + (displayWidth * anim1Diff.x), imageStarting.y - anim1.height + anim1Diff.y, anim1.width, anim1.height); 
+          context.drawImage(anim1, imageStarting.x + (displayWidth * anim1Diff.x), imageStarting.y - anim1RenderDim.height + (displayHeight * anim1Diff.y), anim1RenderDim.width, anim1RenderDim.height); 
         })
   
         anim2.addEventListener('load', ()=> {
+          const anim2AR = anim2.width / anim2.height;
+          const anim2RenderDim  = {width: displayHeight * .1 / anim2AR , height: displayHeight * .1};
           console.log("anim2")
           // drawingContext.fillRect( displayWidth * .15, displayHeight * .45 + imageDim.height , 10,10)
-          context.drawImage(anim2, imageStarting.x + anim2Diff.x , imageStarting.y + imageDim.height + anim2Diff.y, anim2.width, anim2.height); 
+          context.drawImage(anim2, imageStarting.x + (displayWidth * anim2Diff.x) , imageStarting.y + imageDim.height + (displayHeight * anim2Diff.y), anim2RenderDim.width, anim2.height); 
         })
       });
 
@@ -161,18 +157,22 @@ const CanvasComponent: React.FC = () => {
   const clearDrawingCanvas = () => {
     if (drawingContext && drawingCanvasRef.current) {
       drawingContext.clearRect(0, 0, drawingCanvasRef.current.width, drawingCanvasRef.current.height);
-      // finalStartPoint.x = startPoint.x * (displayWidth / imageDim.width)
-      // finalStartPoint.y = startPoint.y * (displayHeight / imageDim.height)
+      finalStartPoint = {x: initialStartPoint.x, y: initialStartPoint.y};
     }
   };
   
   async function handleGestureEvent(event: any) {
     let { nativeEvent: { absoluteX, absoluteY, state } } = event;
-    absoluteX = absoluteX * .8;
-    absoluteY = absoluteY * .8;
+    if(!((finalStartPoint.x-touchDistance < absoluteX && absoluteX < (finalStartPoint.x) + touchDistance) && (finalStartPoint.y-touchDistance < absoluteY && absoluteY < (finalStartPoint.y + touchDistance)))) {
+      isDrawingStop = false;
+      isDrawing = false;
+      return;
+    } ;
+
+    // absoluteX = absoluteX * .8;
+    // absoluteY = absoluteY * .8;
     let x = (xyDiff.x + absoluteX);
     let y = (xyDiff.y + absoluteY);
-    console.log("final x: y", finalStartPoint.x, finalStartPoint.y)
     switch (state) {
       case State.ACTIVE:
         if(isDrawingStop) return;
@@ -188,7 +188,7 @@ const CanvasComponent: React.FC = () => {
           drawingContext.lineTo(x, y);
           drawingContext.stroke()     
           finalStartPoint.x = x
-          finalStartPoint.y = y       
+          finalStartPoint.y = y   
         }
         isDrawing = true;
       }
@@ -197,31 +197,36 @@ const CanvasComponent: React.FC = () => {
           console.log("x:y", x, y)
           const finalX = x * PixelRatio.get();
           const finalY = y * PixelRatio.get();
-          const pixelData = (await backgroundContext.getImageData(finalX, finalY, 1, 1)).data;
+          const pixelData = (await backgroundContext.getImageData(finalX, finalY, 3, 3)).data;
           console.log(pixelData)
           const color = `rgba(${pixelData[0]}, ${pixelData[1]}, ${pixelData[2]}, ${pixelData[3] / 255})`;
           const colorHex = rgbaToHex(pixelData[0], pixelData[1], pixelData[2], pixelData[3] / 255).toLowerCase()
-          // if(PATH_COLOR.toLowerCase() == colorHex.toLowerCase()) {
-          //   // console.log("*************************** match found ******************************");
-          //   if( finalEndPoint.x <= x && x <= (finalEndPoint.x + finalEndPoint.w) &&  finalEndPoint.y <= y && y <= (finalEndPoint.y + finalEndPoint.h)) {
-          //     console.log("***game completed***")
-          //     isDrawingStop=true;
-          //   }
-          // } else {
-          //   console.log("else x:y", x, y)
-          //   console.log("color hex: ", colorHex)
-          //   console.log("****game over*****");
-          //   isDrawingStop = true;
-          //   clearDrawingCanvas();
-          // }
+          if(PATH_COLOR.toLowerCase() == colorHex.toLowerCase()) {
+            // console.log("*************************** match found ******************************");
+            if( finalEndPoint.x <= x && x <= (finalEndPoint.x + finalEndPoint.w) &&  finalEndPoint.y <= y && y <= (finalEndPoint.y + finalEndPoint.h)) {
+              Toast.show(
+                'Game Completed',
+                Toast.SHORT
+              );
+              isDrawingStop=true;
+            }
+          } else {
+            isDrawingStop = true;
+            clearDrawingCanvas();
+            Toast.show(
+              'Game Over',
+              Toast.SHORT
+            );
+          }
           // drawCircle(color);
       } catch (error) {
           console.error("Error getting pixel data:", error);
       }
       }
-      break;
 
+      break;
       case State.END:
+        console.log("End")
         isDrawingStop = false;
         isDrawing = false;
         break;
@@ -251,7 +256,7 @@ const CanvasComponent: React.FC = () => {
         <Button title="Clear Canvas" onPress={clearDrawingCanvas} />
       </View>
       <View style={styles.title}> 
-        <Text style={styles.titleText}>Help Meghuchi find the way to the dragonfly</Text>
+        <Text style={styles.titleText}>{Title}</Text>
       </View>
     </GestureHandlerRootView>
   );
@@ -266,9 +271,6 @@ const styles = StyleSheet.create({
   backgroundCanvas: {
     zIndex: 0,
   },
-  drawingCanvas: {
-    zIndex: 1,
-  },
   controls: {
     position: 'absolute',
     bottom: 0,
@@ -282,14 +284,13 @@ const styles = StyleSheet.create({
     position: 'absolute',
     top: "8%",
     left: "10%",
-    right: 0,
-    alignItems: 'center',
     padding: 10,
     maxWidth: "80%",
     borderWidth: 1,
-    borderRadius: 30,
+    borderRadius: 20,
     justifyContent: "center",
     borderColor: '#C98936',
+    alignItems: 'center',
     backgroundColor: '#009245',
   }, 
   titleText: {
